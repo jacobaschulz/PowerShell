2b0081d3e7ed4910a8c49001495244953486f219
Jacob Schulz <jacobaschulz@yahoo.com>
2019-11-15 23:51:03 -0800

    Added Get-RemoteAppLogs function and script to call module.

---
+++ Functions/Get-RemoteAppLogs.ps1
@@ -0,0 +1,9 @@
+﻿Function Get-RemoteAppLogs {
+    param (
+    [string] $Computer,
+    [string] $Source,
+    [int] $Entries
+    )
+
+    Get-EventLog -LogName Application -ComputerName $Computer -Source $Source -Newest $Entries
+    }
---
+++ Wndows-Logs/Get App Logs - All Sources.ps1
@@ -0,0 +1,6 @@
+﻿Import-Module "C:\Users\$env:username\GitHub\Powershell-Scripts\Functions\Get-RemoteAppLogs.ps1"
+
+# Number of logs to get
+$NumLogs = 15
+
+Get-RemoteLogs -Computer $env:COMPUTERNAME -Source * -Entries $NumLogs
