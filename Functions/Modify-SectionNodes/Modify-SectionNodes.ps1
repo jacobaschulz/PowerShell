2df0493274bd5afba699f319040cc84725aa0e06
Jacob Schulz <jaschulz0705@gmail.com>
2020-10-26 22:04:07 -0700

    Add files via upload
    Added Modify-SectionNodes.ps1 and database-config.xml

---
+++ Functions/Modify-SectionNodes/Modify-SectionNodes.ps1
@@ -0,0 +1,25 @@
+﻿
+# Varaible configurations
+$filepath = "C:\database-config.xml"
+
+# Create a function to modify the nodes in sections.section of the XML
+Function Modify-SectionNodes {
+Param (
+    [string] $key,
+    [string] $value
+	)
+	$file = "$filepath"
+	$config = (Get-content -Path $file) -as [xml]
+	$node = ($config.sections.section).ChildNodes # Gets the childnodes in sections.section of the XML
+	$node_key = $node | where -Property key -EQ "$key" # Chooses the propery "key" of the XML
+	$node_key.value = "$value" # Changes the current value to a new value
+
+	# Saves the config file after changes are made
+	$config.Save($file)
+	}
+
+# Change the database value
+Modify-SectionNodes -key "database" -value "new_database"
+
+# Change the server value
+Modify-SectionNodes -key "server" -value "localhost:8080"
