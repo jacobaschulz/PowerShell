2ad0cf216d62376c915f85003b8525c73c922072
Jacob Schulz <jacobaschulz@yahoo.com>
2021-05-02 09:16:50 -0700

    Delete Wndows-Logs directory

--- Wndows-Logs/Get App Logs - All Sources.ps1
+++
@@ -1,6 +0,0 @@
-﻿Import-Module "C:\Users\$env:username\GitHub\Powershell-Scripts\Functions\Get-RemoteAppLogs.ps1"
-
-# Number of logs to get
-$NumLogs = 15
-
-Get-RemoteLogs -Computer $env:COMPUTERNAME -Source * -Entries $NumLogs
