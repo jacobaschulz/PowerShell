845f9061ee76707bf41ba4b3dde3863389b20161
Jacob Schulz <jacobaschulz@yahoo.com>
2021-05-02 09:11:13 -0700

    Update Modify-SectionNodes.ps1
    Updated comments.

--- Functions/Modify-SectionNodes/Modify-SectionNodes.ps1
+++ Functions/Modify-SectionNodes/Modify-SectionNodes.ps1
@@ -11,15 +11,15 @@ Param (
 	$file = "$filepath"
 	$config = (Get-content -Path $file) -as [xml]
 	$node = ($config.sections.section).ChildNodes # Gets the childnodes in sections.section of the XML
-	$node_key = $node | where -Property key -EQ "$key" # Chooses the propery "key" of the XML
+	$node_key = $node | where -Property key -EQ "$key" # Chooses the property "key" of the XML
 	$node_key.value = "$value" # Changes the current value to a new value
 
 	# Saves the config file after changes are made
 	$config.Save($file)
 	}
 
-# Change the database value
+# Change the database value to a new value
 Modify-SectionNodes -key "database" -value "new_database"
 
-# Change the server value
+# Change the server value to a new value
 Modify-SectionNodes -key "server" -value "localhost:8080"
