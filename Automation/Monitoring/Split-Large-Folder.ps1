ff49568bb93bdca5e5976028c8c65192973e3166
Jacob Schulz <jaschulz0705@gmail.com>
2021-08-22 10:57:36 -0700

    Create Split-Large-Folder.ps1

---
+++ Automation/Monitoring/Split-Large-Folder.ps1
@@ -0,0 +1,52 @@
+$Import = 'C:\Users\jacob\Documents\Web-Split-File-Test\Import'
+$Final = 'C:\Users\jacob\Documents\Web-Split-File-Test\Final'
+$SubImport = get-childitem $Import\*
+$Extension = ''
+
+Foreach($Subfolder in $SubImport) {
+    Get-ChildItem -Path $Subfolder -Recurse -File | Where-Object {$_.Extension -eq $Extension} |
+        Rename-Item -NewName {$_.BaseName + "-" + (Get-Date -Format "yyyyMMdd-HHmmssffff") + $_.Extension}
+        } 
+
+# Move files out to parent
+Foreach($Subfolder in $SubImport) {
+    Get-ChildItem -Path $Subfolder -Recurse -File | Where-Object {$_.Extension -eq $Extension} |
+        Move-Item -Destination $Subfolder -ErrorAction SilentlyContinue
+        }
+
+# Delete empty folders
+Foreach($Subfolder in $SubImport) {
+    Get-ChildItem -Path $Subfolder -Recurse -Directory -Exclude *.* | Remove-Item -Recurse 
+    }
+
+# Folder Reorganization
+$FilesPerFolder = 2
+$inc = 0;
+$FolderNumber = 1;
+
+Foreach($Subfolder in $SubImport) {
+    Get-ChildItem -Path "$Subfolder\*" | Where-Object {$_.Extension -eq $Extension} |
+    Foreach {
+
+    New-Item -Path "$Subfolder\Reorganized-Files-$FolderNumber" -ItemType Directory -Force | Out-Null
+    Move-Item -Path $_ "$Subfolder\Reorganized-Files-$FolderNumber" -Force;
+
+    $inc++;
+
+    if($inc -eq $FilesPerFolder){
+        $FolderNumber++;
+        $inc = 0;
+        }
+    }
+}
+
+
+# Move reorganized folders
+Foreach($Subfolder in $SubImport) {
+    If(
+    Get-ChildItem -Path $Subfolder -Recurse -Directory | Where-Object {$_.Name -match "Reorganized-Files"}) {
+        Move-Item -Destination $Final -Path $Subfolder}
+        Else {
+        Start-Sleep -Seconds 5
+        }
+    }
